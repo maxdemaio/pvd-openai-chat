10a0e40e0d09058039a7a00668da8c0863acddd1
maxdemaio <maxdman321@gmail.com>
2023-03-22 22:55:25 -0400

    finish chatbot

--- client/app/index.js
+++ client/app/index.js
@@ -1,18 +1,126 @@
-async function handleSayHello(e) {
-    e.preventDefault();
-    const nameInput = document.getElementById('name');
-    const name = document.getElementById('name').value;
-    const response = await fetch('/api/hello', {
+function removeLoadingIndicatorFromDialogueBox() {
+    // get the loading indicator element
+    const loadingIndicator = document.getElementById('loading-indicator');
+
+    // remove the loading indicator from the DOM
+    loadingIndicator.remove();
+}
+
+function addBotMessageToDialogueBox(response) {
+	// remove the loading indicator now that the response has been received
+    	removeLoadingIndicatorFromDialogueBox();
+    
+    // create a new li element
+    const botMessage = document.createElement('li');
+
+    // style the bot response element based on the status
+    if (response.status === "error") {
+        // add error styling
+        botMessage.classList.add('bg-red-500', 'text-white', 'rounded', 'p-2', 'w-fit', 'self-start');
+
+        // add error text
+        botMessage.innerText = "Oh no! Something went wrong. Please try again later.";
+    } else {
+        // add user-specific styling to element
+        botMessage.classList.add('bg-gray-500', 'text-white', 'rounded', 'p-2', 'w-fit', 'self-start');
+
+        // add the user's response to the element
+        botMessage.innerText = response.data.trim();
+    }
+
+    // add the li element to the DOM
+    document.getElementById('dialogue').appendChild(botMessage);
+
+    // clear the input for the next response
+    document.getElementById('prompt-input').value = '';
+}
+
+function addLoadingIndicatorToDialogueBox() {
+    // create a new li element
+    const loadingIndicator = document.createElement('li');
+
+    // set the id of the loading indicator
+    loadingIndicator.id = 'loading-indicator';
+
+    // add loading indicator styling
+    loadingIndicator.classList.add('bg-gray-500', 'text-white', 'rounded', 'p-2', 'w-fit', 'self-start', 'w-12');
+
+    // create a new image element
+    const loadingImage = document.createElement('img');
+
+    // set the image source
+    loadingImage.src = '../images/loading.svg';
+
+    // add loading indicator image as a child to li element
+    loadingIndicator.appendChild(loadingImage);
+
+    // add the li element to the DOM
+    document.getElementById('dialogue').appendChild(loadingIndicator);
+}
+
+// accept the user's message as a function input
+function addUserMessageToDialogueBox(message) {
+    // create a new li element
+	const userMessage = document.createElement('li');
+
+    // add user-specific styling to element
+    // so that it's obvious this is the user's prompt 
+    // and not a response from the chatbot
+    userMessage.classList.add('bg-indigo-500', 'text-white', 'rounded', 'p-2', 'w-fit', 'self-end', 'break-words');
+
+    // add the user's message to the li element
+    userMessage.innerText = message;
+
+    // add the li element to the DOM
+    document.getElementById('dialogue').appendChild(userMessage);
+
+    // clear the input for the next question
+    document.getElementById('prompt-input').value = '';
+    
+    // display loading indicator in dialogue box
+    addLoadingIndicatorToDialogueBox();
+}
+
+// function accepts the user `message` as an input
+async function handleSubmitMessage(message) {
+    // input validation
+    if (!message) {
+        return alert('Please enter your support question');
+    }
+
+    // add the user's message to the DOM
+    // (we'll implement this later)
+	addUserMessageToDialogueBox(message);
+
+    // send fetch request to our backend api
+    const response = await fetch('/api/openai', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name }),
+        body: JSON.stringify({ message }),
     });
+    
+    // parse our server's response as json
+    const payload = await response.json();
 
-    nameInput.value = '';
-    const { data } = await response.json();
-    alert(data);
+    // return the response
+    return payload
 }
 
-document.getElementById('name-form').addEventListener('submit', handleSayHello);
+// when the window loads, attach an event listener to the form
+// that fires when the 'send' button is clicked
+window.onload = () => document.getElementById('prompt-form').addEventListener('submit', (e) => {
+    // prevent the form from refreshing the page
+    e.preventDefault();
+
+    // get the value of the input
+    const message = document.getElementById('prompt-input').value;
+
+    // call the function that handles the fetch request
+    handleSubmitMessage(message).then((data) => {
+        // add the chatbot's response to the DOM 
+        // when the fetch request is complete
+		addBotMessageToDialogueBox(data);
+    });
+});
--- server/main.js
+++ server/main.js
@@ -16,6 +16,83 @@ app.use(bodyParser.json())
 // serve static files from client folder (js, css, images, etc.)
 app.use(express.static(path.join(process.cwd(), 'client')))
 
+// define a list of messages to be used as context for the chat
+// using the global scope that way it can be used elsewhere on the server, if needed
+// (this use of `global` shouldn't be done in a "real" app)
+global.messages = [
+    {
+        role: "system",
+        content: "You are a helpful, empathetic, and friendly customer support specialist. You are here to help customers with their orders. You sometimes make small talk."
+    },
+    {
+        role: "system",
+        content: "Additionally, you never ask the customer to upload or provide any photos as our website has no means of doing so at this time. Also, do not mention that you are a bot."
+    },
+];
+
+
+// our route `/api/openai` is arbitrary
+// but it's generally a convention to prefix routes with `/api` 
+// followed by a descriptive resource name, in our case `/openai`
+
+// create http post endpoint that accepts user input
+// and sends it to OpenAI Chat API
+// then returns the response to the client
+app.post('/api/openai', async (req, res) => {
+    const { message } = req.body;
+
+    // store user message in global message state
+    const userMessage = { role: "user", content: message };
+
+    // add to global messages list
+    global.messages.push(userMessage);
+
+    // send a request to the OpenAI API with the user's prompt
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        // construct the request payload
+        // using the entire chat history (global.messages)
+        // sending an external request to the OpenAI API
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: global.messages,
+            // the maximum number of tokens/words the bot should return
+            // in response to a given prompt
+            max_tokens: 100,
+        }),
+    });
+
+    if (!response.ok) {
+        // if the request was not successful, parse the error
+        const error = await response.json();
+
+        // log the error for debugging purposes
+        console.error('OpenAI API Error:', error);
+
+        // return an error response to the client
+        return res.json({ status: 'error', data: null });
+    }
+
+    // parse the response from OpenAI as json
+    const data = await response.json();
+
+    // get the bot's answer from the OpenAI API response
+    const botAnswer = data?.choices?.[0]?.message?.content
+
+    // create the bot message object
+    const botMessage = { role: "assistant", content: botAnswer };
+
+    // store bot message in global message state
+    global.messages.push(botMessage);
+
+    // send the bot's answer back to the client
+    return res.json({ status: 'success', data: botAnswer });
+});
+
 // create http post endpoint that accepts user input
 app.post('/api/hello', async (req, res) => {
     const { name } = req.body;
